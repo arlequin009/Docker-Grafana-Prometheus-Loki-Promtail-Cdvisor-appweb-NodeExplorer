3b3ce5708ec84530d0228a90b858d730d7c2b420
arlequin009 <161957157+arlequin009@users.noreply.github.com>
2024-03-08 01:20:50 -0300

    Delete Monitoring Grafana, prometheus, loki directory

--- Monitoring Grafana, prometheus, loki/guardar_comandos_tiempo_real.sh
+++
@@ -1,5 +0,0 @@
-# en comandos de "echo"
-echo '' >> ~/.bashrc
-echo '#guardar los comandos en tiempo real' >> ~/.bashrc
-echo 'shopt -s histappend' >> ~/.bashrc
-echo 'PROMPT_COMMAND="history -a;$PROMPT_COMMAND"' >> ~/.bashrc
